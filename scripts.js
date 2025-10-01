64771dc2e51bee95d1c54f3c6c8b08d31f3597de
Luiz Enrique <luizenrique80025030@gmail.com>
2025-10-01 00:06:27 -0300

    Concertos e melhora na responsividade.

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const channelId = 2978477;
+const url = 'https://api.thingspeak.com/channels/{channelId}/feeds.json?results=2';
+
+freqCard = document.getElementById('freqCard')
+presCard = document.getElementById('presCard')
+oxigen = document.getElementById('oxigen')
+
+async function getData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    dados_atuais = data.feeds[data.feeds.length - 1]
+    console.log(dados_atuais);
+
+    oxigen.innerText = `100`
+    freqCard.innerText = `${dados_atuais.field1}`
+    presCard.innerText = `${dados_atuais.field2}`
+}
+
+setInterval(getData, 20000);
+getData();
