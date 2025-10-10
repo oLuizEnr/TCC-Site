05dedfe00ef200931d1dccd4b0a1afae05bf0f26
Luiz Enrique <luizenrique80025030@gmail.com>
2025-10-10 09:32:35 -0300

    Correção simples de push.

--- scripts.js
+++ scripts.js
@@ -1,20 +1,24 @@
-const channelId = 2978477;
-const url = 'https://api.thingspeak.com/channels/{channelId}/feeds.json?results=2';
+// const channelId = 2978477;
+// const url = 'https://api.thingspeak.com/channels/{channelId}/feeds.json?results=2';
 
-freqCard = document.getElementById('freqCard')
-presCard = document.getElementById('presCard')
-oxigen = document.getElementById('oxigen')
+// freqCard = document.getElementById('freqCard')
+// presCard = document.getElementById('presCard')
+// oxigen = document.getElementById('oxigen')
 
-async function getData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    dados_atuais = data.feeds[data.feeds.length - 1]
-    console.log(dados_atuais);
+// async function getData() {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     dados_atuais = data.feeds[data.feeds.length - 1]
+//     console.log(dados_atuais);
 
-    oxigen.innerText = `100`
-    freqCard.innerText = `${dados_atuais.field1}`
-    presCard.innerText = `${dados_atuais.field2}`
-}
+//     oxigen.innerText = `100`
+//     freqCard.innerText = `${dados_atuais.field1}`
+//     presCard.innerText = `${dados_atuais.field2}`
+// }
 
-setInterval(getData, 20000);
-getData();
+// setInterval(getData, 20000);
+// getData();
+
+fetch("https://seusite.com/api/dados/")
+  .then(res => res.json())
+  .then(data => console.log(data))
