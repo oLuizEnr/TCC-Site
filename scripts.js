c4175676ea7b9268cbbbd78a5d9f3c96e311efab
Luiz Enrique <165702886+oLuizEnr@users.noreply.github.com>
2025-11-19 11:57:35 -0300

    Editando o fetch do frontend

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 // const channelId = 2978477;
 // const url = 'https://api.thingspeak.com/channels/{channelId}/feeds.json?results=2';
 
-// freqCard = document.getElementById('freqCard')
-// presCard = document.getElementById('presCard')
-// oxigen = document.getElementById('oxigen')
+freqCard = document.getElementById('freqCard')
+presCard = document.getElementById('presCard')
+oxigen = document.getElementById('oxigen')
 
 // async function getData() {
 //     const response = await fetch(url);
@@ -12,13 +12,30 @@
 //     console.log(dados_atuais);
 
 //     oxigen.innerText = `100`
-//     freqCard.innerText = `${dados_atuais.field1}`
-//     presCard.innerText = `${dados_atuais.field2}`
+    // freqCard.innerText = `${dados_atuais.field1}`
+    // presCard.innerText = `${dados_atuais.field2}`
 // }
 
 // setInterval(getData, 20000);
 // getData();
 
-fetch("https://seusite.com/api/dados/")
+async function buscarData() {
+  const response = await fetch("https://tcc-site-gclc.onrender.com/enviar/");
+  const dados = await response.json();
+  console.log(dados);
+
+  dados.forEach(mov => {
+    console.log(`Frequência: ${mov.freqCardi}, Pressão: ${mov.presCardi}, Oxigênio: ${mov.oxigen}.`);
+
+    freqCard.innerText = `${mov.freqCardi}`;
+    presCard.innerText = `${mov.presCardi}`;
+    oxigen.innerText = `${mov.oxigen}`;
+  });
+}
+
+setInterval(buscarDados, 2000);
+buscarDados();
+
+fetch("https://tcc-site-gclc.onrender.com/enviar/")
   .then(res => res.json())
   .then(data => console.log(data))
